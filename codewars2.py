4ce7e300eeb5b4786f7651892e3522e109936be4
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-06-25 15:04:07 +0200

    New additions
    New exercises from codewars I have completed

---
+++ codewars2.py
@@ -0,0 +1,21 @@
+# text = input("enter text")
+# duplicate_count = 0
+# for char in text:
+#     a = char
+#     b = 0
+#     for char in text:
+#         if a == char:
+#             b +=1
+#             if b >= len(text):
+#                 duplicate_count += 1
+# print(duplicate_count)
+
+text = input("enter text")
+duplicate_count = 0
+duplicates = ""
+for char in text.casefold():
+    if text.casefold().count(char) > 1:
+        if char not in duplicates:
+            duplicates += char
+            duplicate_count += 1
+print(duplicate_count)
