6c2d357bca4cff8c174c6fe62f068b94ab1ecf24
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-06-25 15:09:00 +0200

    Added exercise url

--- codewars2.py
+++ codewars2.py
@@ -1,3 +1,16 @@
+# https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+
+text = input("enter text")
+duplicate_count = 0
+duplicates = ""
+for char in text.casefold():
+    if text.casefold().count(char) > 1:
+        if char not in duplicates:
+            duplicates += char
+            duplicate_count += 1
+print(duplicate_count)
+
+
 # text = input("enter text")
 # duplicate_count = 0
 # for char in text:
@@ -9,13 +22,3 @@
 #             if b >= len(text):
 #                 duplicate_count += 1
 # print(duplicate_count)
-
-text = input("enter text")
-duplicate_count = 0
-duplicates = ""
-for char in text.casefold():
-    if text.casefold().count(char) > 1:
-        if char not in duplicates:
-            duplicates += char
-            duplicate_count += 1
-print(duplicate_count)
