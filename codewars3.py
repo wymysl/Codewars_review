b38f4f233712f0f5403d3569f48b5f9bb7e923f1
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-06-25 19:09:47 +0200

    Attempt at solving 3rd codewars exercise
    Since the previous code was actually just a solution to
    today's first exercise (used here just for me to understand how git works),
    I tried (and failed) to solve a third exercise today.
    My maths is too rusty or maybe I am just missing something.

--- codewars3.py
+++ codewars3.py
@@ -1,13 +1,48 @@
-print("Please provide a number: ")
-number = int(input())
+# https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/python
+#
+# Way too much maths. Brain hurts. Must rest.
+#
+# This code is a mess and so am I.
+#
+# def tankvol(h, d, vt):
 
-if number < 0:
-    print(0)
+from math import atan, sqrt, pi, degrees, sin
+h = 40
+d = 120
+vt = 3500
 
-total = 0
+r = (d / 2)
 
-for i in range(number):
-    if i % 3 == 0 or i % 5 == 0:
-        total += i
+cylinder_h = vt / (pi * (r ** 2))
 
-print("The sum of all the natural numbers below {} that are multiples of 3 or 5 is {}.".format(number, total))
+chord = 2 * (sqrt((d * h) - (h ** 2)))  # This calculates the base of the right triangle with the height of d - h and an arm of length d/2. I wish I knew how to make this comment line more elegant since it's so long, but I don't know how.
+
+# angle = acos((r ** 2 + r ** 2 - chord ** 2) / ((2 ** r) ** 2))
+
+angle = degrees(atan(chord / ((r - h))))
+
+# area = (1/2 * (angle - sin(angle)) * r ** 2)
+
+area = (0.5 * (r ** 2) * (angle - sin(angle)))
+
+volume = area * cylinder_h
+
+print(volume)
+
+# area_triangle = r ** base
+#
+# angle = degrees(atan(base / ((d / 2) - h))) # This calculates the angle of the circle segment.
+#
+# area_segment = ((angle / 360) ** (pi ** (r ** r)))
+#
+# area_h = area_segment - area_triangle
+# Since my maths is very rusty, only after writing the above code did I realise there is a much more efficient way
+# to find the area of the circle segment minus the triangle:
+
+# area = (0.5 * ((r ** r) * (angle - sin(angle))))
+
+# area = ((r ** r) * acos((r - h) / r) - (r - h) * sqrt(2 ** ((r ** h) - h ** h)))
+#
+# v = int(area ** cylinder_h)
+#
+# print(v)
