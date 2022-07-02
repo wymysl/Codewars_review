20155b3daaa0d98ad6e4c1a4b4e9b47899860c54
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-02 18:21:07 +0200

    Improvements
    Actually I guess the loops are redundant. So I got rid of them.
    (Although I am relieved to find that a solution similar to mine was the third most popular).

--- codewars11.py
+++ codewars11.py
@@ -1,11 +1,7 @@
 # https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
 
 def move_zeros(array):
-    zero_count = []
-    for index, x in enumerate(array):
-        if x == 0:
-            zero_count.append(0)
-
+    zero_count = [value for value in array if value == 0]
     array = [value for value in array if value != 0]
     array.extend(zero_count)
 
