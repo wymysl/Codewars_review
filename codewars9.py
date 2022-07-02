e32e8debd0de28707c0cf15bc413d795f1368fdb
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-02 20:59:24 +0200

    DONE!
    It works! I feel like I only managed to make this work using a few too many workarounds and a pack of bubblegum, but still it does its job. Please note lines 7 and 9 - I am sure there is a better way to do this, but I'm not there yet...

--- codewars9.py
+++ codewars9.py
@@ -1,39 +1,22 @@
 # https://www.codewars.com/kata/54e6533c92449cc251001667/train/python
 # I misunderstood the assignment and wrote code which only prints the first occurence of a character in a string.
 # I will repair it later.
-# def unique_in_order(iterable):
 
-iterable = 'AAAABBBCCDAABBB'
-iterable_new = []
-for index, x in enumerate(iterable):
-    if index in range (len(iterable)):
-        if iterable.count(x) > 1:
-            if iterable[index + 1] == '{}'.format(x):
-                iterable = iterable.replace(iterable[index + 1], '')
-                break
+def unique_in_order(iterable):
+    iterable_new = []
+    iterable += " "
+    for index, x in enumerate(iterable):
+        if index in range(len(iterable) - 1):
+            if iterable.count(x) > 1:
+                if iterable[index + 1] == iterable[index]:
+                    continue
+                else:
+                    iterable_new.append(x)
+            else:
+                iterable_new.append(x)
         else:
-            iterable_new += x
-            iterable = iterable.replace(iterable[index], '')
-    else:
-        break
+            break
+    iterable = iterable_new
+    return iterable
 
-
-print(iterable_new)
-
-
-# iterable = 'AAAABBBCCDAABBB'
-# iterable_new = []
-# for y in iterable:
-#     for x in iterable:
-#         if iterable.count(x) > 1:
-#
-#             iterable_new += x
-#             iterable = iterable.replace(x, '')
-#             break
-#
-#         else:
-#             iterable_new += x
-#             iterable = iterable.replace(x, '')
-#
-#
-# print(iterable_new)
+print(unique_in_order("AAAABBBCCDAABBB"))
