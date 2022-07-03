f1c52ec41892ae345acd5e015c52e31b5808d5ab
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-03 12:41:36 +0200

    Works but doesn't pass the test
    I cleaned the code (I only just noticed that Pycharm shows PEP8 and other suggestions!). I also tweaked the code a bit - now the codewars terminal returns different err ('not all arguments converted during string formatting'). I do not know how to solve this.

--- codewars8.py
+++ codewars8.py
@@ -1,10 +1,11 @@
 # https://www.codewars.com/kata/58b3c2bd917a5caec0000017/train/python
-# I know the code doesn't  work as it should... yet. I will fix it later.
+# works locally, but codewars returns error:/
+# not all arguments converted during string formatting (line 17 I think)
 
 def sum_groups(arr):
     arr.append("hehe")
-    Batman = True
-    while Batman:
+    batman = True
+    while batman:
         init_len = str(len(arr))
         for index, x in enumerate(arr):
             if arr[index] % 2 == 0:
@@ -16,7 +17,6 @@ def sum_groups(arr):
                             arr[index] += arr[index + 1]
                             arr.pop(index + 1)
                 else:
-                    arr.pop(-1)
                     break
             else:
                 if arr[index + 1] != "hehe":
@@ -28,107 +28,9 @@ def sum_groups(arr):
                     break
         final_len = str(len(arr))
         if init_len == final_len:
+            batman = False
             arr.pop(-1)
-            Batman = False
-    return(len(arr))
-
-    # else:
-    #     if arr[index] % 2 == 0:
-    #         if arr[index + 1] % 2 != 0:
-    #             continue
-    #     else:
-    #         while arr[index + 1] % 2 == 0:
-    #             arr[index] += arr[index + 1]
-    #             arr.pop(index + 1)
+    return len(arr)
 
 
-    return arr
-
-arr = [2, 1, 2, 2, 6, 5, 0, 2, 0, 5, 5, 7, 7, 4, 3, 3, 9]
-print(sum_groups(arr))
-
-
-# items_to_sum = []
-#
-# for index, number in enumerate(arr)
-#     if number % 2 == 0:
-#         if i+1 % 2 == 0:
-#             arr.pop(i)
-
-# items_to_pop = []
-# x = -1
-#
-# for x in arr:
-#     if arr[x] % 2 == 0:
-#         if arr[x - 1] % 2 != 0:
-#             items_to_pop.append([x])
-#         else:
-#             arr[x] += arr[x - 1]
-#             arr.pop(x - 1)
-#     else:
-#         if arr[x - 1] % 2 == 0:
-#             x += 1
-#         else:
-#             arr[x] += arr[x - 1]
-#             items_to_pop.append([x])
-#
-# arr.pop(items_to_pop)
-#
-# print(arr)
-
-# arr_position = n = -1 # for code readability
-# evens = []
-# odds = []
-# for x in arr:
-#     n += 1
-#     if arr[n] % 2 == 0:
-#         evens += [n]
-#     else:
-#         odds += [n]
-#
-# print(evens)
-# print(odds)
-
-
-    #
-    # else:
-    #     if arr[n + 1] % 2 == 0:
-    #         odds = arr[n]
-    #
-    #
-    #
-    #     if arr[n + 1] % 2 != 0:
-    #     arr[n] += arr[arr_position + 1]
-    #     del arr[n + 1]
-    #     n += 1
-    #
-    # and arr[n + 1] % 2 == 0) or (arr[n] % 2 != 0 and arr[n + 1] % 2 != 0):
-    #     arr[n] += arr[n + 1]
-    #     del arr[n + 1]
-    #     n += 1
-    # else:
-    #     n += 1
-
-# print(arr)
-#
-#
-# # for x in arr:
-#     if arr[x] % 2 != 0:
-#         if arr[x + 1] % 2 == 0:
-#             arr[x] += arr[x + 1]
-#         else:
-#             continue
-#     if arr[x] % 2 == 0:
-#         if arr[x + 1] % 2 != 0:
-#             arr[x] += arr[x + 1]
-#         else:
-#             continue
-#
-# print(arr)
-
-#     if (arr[i] % 2 == 0 and arr [i + 1] % 2 == 0) or (arr[i] % 2 != 0 and arr[i + 1] % 2 != 0):
-#         arr[i] += arr[i + 1]
-#     else:
-#         continue
-#
-# print(arr)
+print(sum_groups([2, 1, 2, 2, 6, 5, 0, 2, 0, 5, 5, 7, 7, 4, 3, 3, 9]))
