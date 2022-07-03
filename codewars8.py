111b23051887400c7c752ec7cd98dbd009b1b01f
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-03 12:12:45 +0200

    Works but doesn't pass the test
    I changed the code. It works locally, but the codewars test doesn't seem to accept my 'list index out of range' workaround. I agree it's rather primitive, but I'm out of ideas. Please advise.

--- codewars8.py
+++ codewars8.py
@@ -1,27 +1,53 @@
 # https://www.codewars.com/kata/58b3c2bd917a5caec0000017/train/python
 # I know the code doesn't  work as it should... yet. I will fix it later.
-# def sum_groups(arr):
+
+def sum_groups(arr):
+    arr.append("hehe")
+    Batman = True
+    while Batman:
+        init_len = str(len(arr))
+        for index, x in enumerate(arr):
+            if arr[index] % 2 == 0:
+                if arr[index + 1] != "hehe":
+                    if arr[index + 1] % 2 != 0:
+                        continue
+                    else:
+                        while arr[index + 1] % 2 == 0:
+                            arr[index] += arr[index + 1]
+                            arr.pop(index + 1)
+                else:
+                    arr.pop(-1)
+                    break
+            else:
+                if arr[index + 1] != "hehe":
+                    while arr[index + 1] % 2 != 0:
+                        arr[index] += arr[index + 1]
+                        arr.pop(index + 1)
+                        break
+                else:
+                    break
+        final_len = str(len(arr))
+        if init_len == final_len:
+            arr.pop(-1)
+            Batman = False
+    return(len(arr))
+
+    # else:
+    #     if arr[index] % 2 == 0:
+    #         if arr[index + 1] % 2 != 0:
+    #             continue
+    #     else:
+    #         while arr[index + 1] % 2 == 0:
+    #             arr[index] += arr[index + 1]
+    #             arr.pop(index + 1)
+
+
+    return arr
 
 arr = [2, 1, 2, 2, 6, 5, 0, 2, 0, 5, 5, 7, 7, 4, 3, 3, 9]
-
-for index, x in enumerate(arr):
-    if arr[x] % 2 == 0:
-        if arr[x + 1] % 2 != 0:
-            continue
-        else:
-            arr[x] += arr[x + 1]
-            arr.pop(x + 1)
-
-    else:
-        if arr[x + 1] % 2 == 0:
-            continue
-        else:
-            arr[x] += arr[x + 1]
-            arr.pop(x + 1)
+print(sum_groups(arr))
 
 
-print(arr)
-print(len(arr))
 # items_to_sum = []
 #
 # for index, number in enumerate(arr)
