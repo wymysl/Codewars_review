2a920bb5b9936519e51716fd3a45509e2c426256
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-13 12:56:35 +0200

    Code revised - ALMOST works
    I rewrote the code, it passes most of the tests, but there are a few instances where it supposedly does not... I don't know why (Codewars' error prompt says e.g.: 'It should work for random inputs too: 269 should equal 268').

--- codewars8.py
+++ codewars8.py
@@ -1,36 +1,21 @@
 # https://www.codewars.com/kata/58b3c2bd917a5caec0000017/train/python
-# works locally, but codewars returns error:/
-# not all arguments converted during string formatting (line 17 I think)
 
 def sum_groups(arr):
-    arr.append("hehe")
-    batman = True
-    while batman:
-        init_len = str(len(arr))
+    while True:
+        init = len(arr)
         for index, x in enumerate(arr):
-            if arr[index] % 2 == 0:
-                if arr[index + 1] != "hehe":
-                    if arr[index + 1] % 2 != 0:
-                        continue
-                    else:
-                        while arr[index + 1] % 2 == 0:
-                            arr[index] += arr[index + 1]
-                            arr.pop(index + 1)
-                else:
-                    break
-            else:
-                if arr[index + 1] != "hehe":
-                    while arr[index + 1] % 2 != 0:
+            if index < len(arr) - 1:
+                if arr[index] % 2 == 0 and arr[index + 1] % 2 == 0 or arr[index] % 2 != 0 and arr[index + 1] % 2 != 0:
                         arr[index] += arr[index + 1]
                         arr.pop(index + 1)
-                        break
-                else:
-                    break
-        final_len = str(len(arr))
-        if init_len == final_len:
-            batman = False
-            arr.pop(-1)
-    return len(arr)
+            elif index == len(arr):
+                if arr[index - 1] % 2 == 0 and arr[index] == 0 or arr[index - 1] % 2 != 0 and arr[index] != 0:
+                    arr[index - 1] += arr[index]
+                    arr.pop(index)
+        end = len(arr)
+        if init == end:
+            break
 
+    return arr
 
 print(sum_groups([2, 1, 2, 2, 6, 5, 0, 2, 0, 3, 3, 3, 9, 2]))
